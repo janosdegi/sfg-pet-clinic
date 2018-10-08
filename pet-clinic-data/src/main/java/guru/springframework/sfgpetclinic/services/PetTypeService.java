9d50a177fda459becc6c7cdbdd283efb3311de05
Dégi János <johann.degi@gmail.com>
2018-10-08 09:48:27 +0200

    improve save() in OwnerMapService

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by Dégi János on 2018.09.14..
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
