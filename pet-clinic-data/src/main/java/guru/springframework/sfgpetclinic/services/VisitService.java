1277e01191511b08d529059719906b4b24cbc443
Dégi János <johann.degi@gmail.com>
2018-10-09 09:39:15 +0200

    Implemented Map Impl for Visit Service

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * Created by Dégi János on 2018.09.14..
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
