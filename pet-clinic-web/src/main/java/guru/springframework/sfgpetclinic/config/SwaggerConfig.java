1ded68685d75ad4d8cfa8aac9e7f00430c938641
Dégi János <johann.degi@gmail.com>
2018-10-17 13:20:16 +0200

    adding swagger to the project

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package guru.springframework.sfgpetclinic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by jd on 2018.10.17..
+ */
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {//} extends WebMvcConfigurationSupport {
+
+    //http://localhost:8080/swagger-ui.html
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/");
+    }
+
+    // without spring boot: if you add this configuration in, swagger ui will be displayed normally for you
+
+//    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("swagger-ui.html")
+//                .addResourceLocations("classpath:/META-INF/resources/");
+//
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
+
+}
