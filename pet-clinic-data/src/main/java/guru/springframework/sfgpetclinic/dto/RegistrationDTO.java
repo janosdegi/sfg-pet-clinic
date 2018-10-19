98839b66ada5e82f3f102dc1906dd38e72021b00
Dégi János <johann.degi@gmail.com>
2018-10-19 09:01:45 +0200

    customizing swagger ui with metadata

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/dto/RegistrationDTO.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/dto/RegistrationDTO.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgpetclinic.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,8 +15,11 @@ import lombok.Setter;
 @NoArgsConstructor
 public class RegistrationDTO {
 
+    @ApiModelProperty(value = "This is the email", required = true)
     private String email;
     private String password;
+
+    @ApiModelProperty(value = "This is the date of registration", required = false)
     private String registrationDate;
 
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/RegistrationController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/RegistrationController.java
@@ -1,6 +1,8 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.sfgpetclinic.dto.RegistrationDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+@Api(description = "This is my registration controller")
 @RestController
 public class RegistrationController {
 
+    @ApiOperation(value = "This is a registration service", notes = "These are some notes about API.")
     @RequestMapping(value = "/registration", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody List<RegistrationDTO> registerUser(RegistrationDTO registrationDTO) {
         RegistrationDTO regDTO = new RegistrationDTO("test@test.com", "123456", "01.01.2019");
